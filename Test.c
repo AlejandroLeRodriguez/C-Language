25c14cec2d0a4ce2598ee1b14ef76379ba86a9ee
AlejandroRodriguez36 <dilpreet_chana@icloud.com>
2014-05-27 17:54:10 -0400

    Create Test.c

---
+++ Test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+	int num;
+	
+	printf("hello there, please enter a number: ");
+	
+	scanf("%d", &num);
+	printf("The number you entered was, %d", num);
+	
+	return 0;
+}
